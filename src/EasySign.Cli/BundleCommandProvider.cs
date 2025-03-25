d4d855245a9baad02e85f1e9cec9f6c13385a32f
Alireza Poodineh <itsaeliux@gmail.com>
2025-03-25 14:19:39 +0330

    Add logger to cli

--- src/EasySign.Cli/BundleCommandProvider.cs
+++ src/EasySign.Cli/BundleCommandProvider.cs
@@ -5,15 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Extensions.Logging;
+
 using SAPTeam.EasySign.CommandLine;
 
 namespace SAPTeam.EasySign.Cli
 {
     internal class BundleCommandProvider : CommandProvider<Bundle>
     {
+        ILogger _logger;
+
+        public BundleCommandProvider(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public override void InitializeBundle(string bundlePath)
         {
-            Bundle = new Bundle(bundlePath);
+            Bundle = new Bundle(bundlePath, _logger);
         }
 
         public override RootCommand GetRootCommand()
--- src/EasySign.Cli/Program.cs
+++ src/EasySign.Cli/Program.cs
@@ -5,6 +5,11 @@ using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Serilog;
+
 using Spectre.Console;
 
 namespace SAPTeam.EasySign.Cli
@@ -13,8 +18,28 @@ namespace SAPTeam.EasySign.Cli
     {
         static int Main(string[] args)
         {
-            var root = new BundleCommandProvider().GetRootCommand();
-            return root.Invoke(args);
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/log-.txt"), rollingInterval: RollingInterval.Day, shared: true)
+                .MinimumLevel.Debug() // Minimum log level
+                .CreateLogger();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(configure =>
+            {
+                configure.ClearProviders(); // Clear default providers
+                configure.AddSerilog();
+            });
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Resolve an ILogger instance
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+            logger.BeginScope("EasySign.Cli started at " + DateTime.Now);
+
+            var root = new BundleCommandProvider(logger).GetRootCommand();
+            var exitCode = root.Invoke(args);
+
+            return exitCode;
         }
 
     }
