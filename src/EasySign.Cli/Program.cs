9e3e5530261f79c8aba0203e72684981fb3239ba
Alireza Poodineh <itsaeliux@gmail.com>
2025-04-13 13:54:26 +0330

    Improve cli startup and logging

--- src/EasySign.Cli/Program.cs
+++ src/EasySign.Cli/Program.cs
@@ -7,6 +7,7 @@ using Serilog;
 using Serilog.Extensions.Logging;
 using SAPTeam.EasySign.CommandLine;
 using SAPTeam.CommonTK;
+using Spectre.Console;
 
 namespace SAPTeam.EasySign.Cli
 {
@@ -23,13 +24,13 @@ namespace SAPTeam.EasySign.Cli
                 .WriteTo.File(
                     Path.Combine(AppDirectory, "logs/log-.txt"),
                     rollingInterval: RollingInterval.Day,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Context}({ThreadId}) - {Message} {NewLine}{Exception}"
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Context}({ThreadId}): {Message} {NewLine}{Exception}"
                 )
                 .MinimumLevel.Debug() // Minimum log level
                 .CreateLogger();
 
             Serilog.ILogger appLogger = Log.Logger.ForContext("Context", "Main");
-            appLogger.Information("Starting EasySign CLI at {DateTime}", DateTime.Now);
+            appLogger.Information("Starting EasySign CLI");
 
             Microsoft.Extensions.Logging.ILogger bundleLogger = new SerilogLoggerFactory(Log.Logger.ForContext("Context", "Bundle"))
                 .CreateLogger("CommandProvider");
@@ -39,28 +40,61 @@ namespace SAPTeam.EasySign.Cli
 
             if (!Directory.Exists(AppDirectory))
             {
+                appLogger.Debug("Creating application data directory at {AppDirectory}", AppDirectory);
                 Directory.CreateDirectory(AppDirectory);
             }
 
-            CommandProviderConfiguration config;
+            CommandProviderConfiguration? config = null;
             if (File.Exists(ConfigPath))
             {
-                using FileStream fs = File.OpenRead(ConfigPath);
-                config = JsonSerializer.Deserialize(fs, typeof(CommandProviderConfiguration), SourceGenerationConfigurationContext.Default) as CommandProviderConfiguration ?? new CommandProviderConfiguration();
+                appLogger.Information("Loading configuration from {ConfigPath}", ConfigPath);
+                FileStream fs = File.OpenRead(ConfigPath);
+
+                try
+                {
+                    config = JsonSerializer.Deserialize(fs, typeof(CommandProviderConfiguration), SourceGenerationConfigurationContext.Default) as CommandProviderConfiguration ?? new CommandProviderConfiguration();
+                    fs.Dispose();
+                }
+                catch
+                {
+                    fs.Dispose();
+
+                    appLogger.Warning("Failed to load configuration from {ConfigPath}", ConfigPath);
+                    config = null;
+
+                    appLogger.Information("Creating backup of the old configuration file at {ConfigPath}.old", ConfigPath + ".old");
+                    File.Copy(ConfigPath, ConfigPath + ".old", true);
+
+                    appLogger.Information("Deleting the broken configuration file at {ConfigPath}", ConfigPath);
+                    File.Delete(ConfigPath);
+
+                    AnsiConsole.MarkupLine($"[yellow]Failed to load configuration file. A backup has been created at {ConfigPath + ".old"}[/]");
+                    AnsiConsole.MarkupLine("[yellow]A new configuration file will be created with default values.[/]");
+                }
             }
-            else
+
+            if (config == null)
             {
+                appLogger.Information("Loading default configuration");
                 config = new CommandProviderConfiguration();
             }
 
+            appLogger.Debug("Loading SAP Team trusted certificates");
             config.AddSAPTeamCertificates();
+
+            appLogger.Debug("Initializing command provider");
             var cp = new BundleCommandProvider(config, commandProviderLogger, bundleLogger);
 
+            appLogger.Debug("Loading command line interface");
             RootCommand root = cp.GetRootCommand();
+
+            appLogger.Information("Running command: {command}", args);
             int exitCode = root.Invoke(args);
+            appLogger.Information("Command completed with exit code {exitCode}", exitCode);
 
-            appLogger.Information("Shutting down EasySign CLI at {DateTime} with exit code {ExitCode}", DateTime.Now, exitCode);
+            appLogger.Information("Shutting down EasySign CLI");
 
+            appLogger.Debug("Saving configuration to {ConfigPath}", ConfigPath);
             string data = JsonSerializer.Serialize(config, config.GetType(), SourceGenerationConfigurationContext.Default);
 
             if(File.Exists(ConfigPath))
@@ -72,7 +106,9 @@ namespace SAPTeam.EasySign.Cli
             {
                 fs.Write(Encoding.UTF8.GetBytes(data));
             }
+            appLogger.Debug("Configuration saved to {ConfigPath}", ConfigPath);
 
+            appLogger.Debug("Application shutdown successfully completed");
             Log.CloseAndFlush();
             return exitCode;
         }
